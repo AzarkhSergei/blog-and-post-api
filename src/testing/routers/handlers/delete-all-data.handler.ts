c5feb1d119ecca11e93fdd1a0ad8099c7b6bb0ef
AzarkhSergei <sergei.azarkh@gmail.com>
2025-11-18 20:37:55 +0200

    fix: migrate database

--- src/testing/routers/handlers/delete-all-data.handler.ts
+++ src/testing/routers/handlers/delete-all-data.handler.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { HttpStatus } from "../../../core/types/http-statuses";
 import { blogCollection, postCollection } from "../../../db/mongo.db";
 
-export async function deleteAllDataHandler(req: Request, res: Response) {
+export async function deleteAllDataHandler(_req: Request, res: Response) {
   await blogCollection.deleteMany();
   await postCollection.deleteMany();
   res.sendStatus(HttpStatus.NoContent);
